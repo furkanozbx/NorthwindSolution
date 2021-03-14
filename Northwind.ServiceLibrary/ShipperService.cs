940f2fff53a7c6d58c6bc78b3602346e3f8a6b78
Furkan Özbay <furkanozb37@hotmail.com>
2021-03-14 03:27:51 +0300

    Add project files.

---
+++ Northwind.ServiceLibrary/ShipperService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Entities;
+
+namespace Northwind.ServiceLibrary
+{
+
+    public class ShipperService : IShipperService
+    {
+        public List<Shipper> GetShippers()
+        {
+            using (NorthwindEntities db=new NorthwindEntities ())
+            {
+                return db.Shippers.ToList();
+            }
+        }
+    }
+}
